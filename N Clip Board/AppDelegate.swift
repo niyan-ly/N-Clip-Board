9a6c94d148f405285b3780a289062162f39ae1c2
poor-branson <work@poor-branson.info>
2019-09-08 16:24:33 +0800

    Initial Commit

---
+++ N Clip Board/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  N Clip Board
+//
+//  Created by branson on 2019/9/8.
+//  Copyright © 2019 branson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
